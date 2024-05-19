5cfa4598095c5e55ce590278c4733a1df6fcc9a1
CristopherGarcia2000 <usr3587@salesianos-lacuesta.net>
2024-05-19 13:40:53 +0100

    added code

--- src/main/java/com/magicvault/config/SecurityConfig.java
+++ src/main/java/com/magicvault/config/SecurityConfig.java
@@ -4,19 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
+
     @SuppressWarnings("deprecation")
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-                .authorizeRequests(requests -> requests
-                        .requestMatchers("/v3/api-docs/", "/swagger-ui/", "/swagger-ui.html" , "/auth/*").permitAll()
-                    .anyRequest().permitAll());
+            .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Añade esta línea
+            .authorizeRequests(requests -> requests
+                .requestMatchers("/v3/api-docs/", "/swagger-ui/", "/swagger-ui.html", "/auth/*").permitAll()
+                .anyRequest().permitAll());
 
         return http.build();
     }
-    
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Permitir todos los orígenes. En producción, especifica los orígenes permitidos.
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // Permitir envío de credenciales (cookies, headers de autenticación, etc.)
+        
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
--- src/main/java/com/magicvault/requests/SetsRequest.java
+++ src/main/java/com/magicvault/requests/SetsRequest.java
@@ -2,6 +2,7 @@ package com.magicvault.requests;
 
 public class SetsRequest {
     private String name;
+    private String code;
 
     // Getters y Setters
 
@@ -12,4 +13,13 @@ public class SetsRequest {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
 }
