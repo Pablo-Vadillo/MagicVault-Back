e6c08f12c2b880dc217c8b5206115475222a206c
= <=>
2024-05-20 22:48:46 +0100

    fix:"Fixed delete"

--- src/main/java/com/magicvault/controller/CollectionsController.java
+++ src/main/java/com/magicvault/controller/CollectionsController.java
@@ -23,122 +23,137 @@ import com.magicvault.requests.AddRemoveCardRequest;
 
 @RestController
 @RequestMapping("/collections")
-@CrossOrigin(origins = "*",allowedHeaders="*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CollectionsController {
-	
+
 	@Autowired
 	public CollectionsRepository collectionsRepository;
-	
+
 	@PostMapping
-	public ResponseEntity<?> saveCollection(@RequestBody Collections collection){
+	public ResponseEntity<?> saveCollection(@RequestBody Collections collection) {
 		try {
 			Collections collectionsaved = collectionsRepository.save(collection);
-			return new ResponseEntity<Collections>(collectionsaved,HttpStatus.CREATED);
+			return new ResponseEntity<Collections>(collectionsaved, HttpStatus.CREATED);
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@GetMapping
-	public ResponseEntity<?> findAllCollections(){
+	public ResponseEntity<?> findAllCollections() {
 		try {
 			List<Collections> collections = collectionsRepository.findAll();
-			return new ResponseEntity<List<Collections>>(collections,HttpStatus.OK);
+			return new ResponseEntity<List<Collections>>(collections, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@GetMapping(value = "/user/{user}")
-	public ResponseEntity<?> findDecksByUser(@PathVariable("user") String user){
+	public ResponseEntity<?> findDecksByUser(@PathVariable("user") String user) {
 		try {
 			List<Collections> usercollections = collectionsRepository.findByUser(user);
-			return new ResponseEntity<List<Collections>>(usercollections,HttpStatus.OK);
+			return new ResponseEntity<List<Collections>>(usercollections, HttpStatus.OK);
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@PutMapping("/addCard")
-    public ResponseEntity<?> addCardToDeck(@RequestBody AddRemoveCardRequest addCardRequest) {
-        try {
-            Optional<Collections> _collection = collectionsRepository.findByCollectionnameAndUser(addCardRequest.getDeckname(), addCardRequest.getUser());
-            if (_collection.isPresent()) {
-                Collections collection = _collection.get();
-                collection.getCollectionlist().add(addCardRequest.getCardName());
-                collectionsRepository.save(collection);
-                return new ResponseEntity<Collections>(collection, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<String>("No se encontró la colección para el usuario especificado", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-	@DeleteMapping("/removeCard")
-    public ResponseEntity<?> removeCardFromDeck(@RequestBody AddRemoveCardRequest removeCardRequest) {
-        try {
-            Optional<Collections> _collection = collectionsRepository.findByCollectionnameAndUser(removeCardRequest.getDeckname(), removeCardRequest.getUser());
-            if (_collection.isPresent()) {
-                Collections collection = _collection.get();
-                boolean removed = collection.getCollectionlist().remove(removeCardRequest.getCardName());
-                if (removed) {
-                    collectionsRepository.save(collection);
-                    return new ResponseEntity<Collections>(collection, HttpStatus.OK);
-                } else {
-                    return new ResponseEntity<String>("La carta no existe en la colección especificada", HttpStatus.NOT_FOUND);
-                }
-            } else {
-                return new ResponseEntity<String>("No se encontró la colección para el usuario especificado", HttpStatus.NOT_FOUND);
-            }
-        } catch (Exception e) {
-            return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> findDeck(@PathVariable("id") String id){
+	public ResponseEntity<?> addCardToDeck(@RequestBody AddRemoveCardRequest addCardRequest) {
 		try {
-			ObjectId collectionId = new ObjectId(id);
-			Optional<Collections> _collection = collectionsRepository.findById(collectionId);
-			if(_collection.isPresent()) 
-			{
+			Optional<Collections> _collection = collectionsRepository
+					.findByCollectionnameAndUser(addCardRequest.getDeckname(), addCardRequest.getUser());
+			if (_collection.isPresent()) {
 				Collections collection = _collection.get();
-				return new ResponseEntity<Collections>(collection,HttpStatus.OK);
-			} else 
-			{
-				return new ResponseEntity<String>("No existe la Colección",HttpStatus.INTERNAL_SERVER_ERROR);
+				collection.getCollectionlist().add(addCardRequest.getCardName());
+				collectionsRepository.save(collection);
+				return new ResponseEntity<Collections>(collection, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("No se encontró la colección para el usuario especificado",
+						HttpStatus.NOT_FOUND);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<?> deleteDeck(@PathVariable("id") String id)
-	{
+
+	@DeleteMapping("/removeCard")
+	public ResponseEntity<?> removeCardFromDeck(@RequestBody AddRemoveCardRequest removeCardRequest) {
 		try {
-			ObjectId collectionId = new ObjectId(id);
-			collectionsRepository.deleteById(collectionId);
-			return new ResponseEntity<String>("Colección Eliminada",HttpStatus.OK);
+			Optional<Collections> _collection = collectionsRepository
+					.findByCollectionnameAndUser(removeCardRequest.getDeckname(), removeCardRequest.getUser());
+			if (_collection.isPresent()) {
+				Collections collection = _collection.get();
+				boolean removed = collection.getCollectionlist().remove(removeCardRequest.getCardName());
+				if (removed) {
+					collectionsRepository.save(collection);
+					return new ResponseEntity<Collections>(collection, HttpStatus.OK);
+				} else {
+					return new ResponseEntity<String>("La carta no existe en la colección especificada",
+							HttpStatus.NOT_FOUND);
+				}
+			} else {
+				return new ResponseEntity<String>("No se encontró la colección para el usuario especificado",
+						HttpStatus.NOT_FOUND);
+			}
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	@PutMapping(value = "/{id}")
-	public ResponseEntity<?> updateCollection(@PathVariable("id")String id, @RequestBody Collections newCollection){
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> findDeck(@PathVariable("id") String id) {
 		try {
 			ObjectId collectionId = new ObjectId(id);
 			Optional<Collections> _collection = collectionsRepository.findById(collectionId);
-			if(_collection.isPresent()) 
-			{
+			if (_collection.isPresent()) {
+				Collections collection = _collection.get();
+				return new ResponseEntity<Collections>(collection, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("No existe la Colección", HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping("/delete")
+	public ResponseEntity<?> deleteCollection(@RequestBody AddRemoveCardRequest toBeDeleted) {
+		try {
+			Optional<Collections> collectionOpt = collectionsRepository
+					.findByCollectionnameAndUser(toBeDeleted.getDeckname(), toBeDeleted.getUser());
+			if (collectionOpt.isPresent()) {
+				Collections collection = collectionOpt.get();
+				collectionsRepository.delete(collection);
+				return new ResponseEntity<Collections>(collection, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("No se encontró la colección para el usuario especificado",
+						HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Error interno del servidor: " + e.getMessage(),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<?> updateCollection(@PathVariable("id") String id, @RequestBody Collections newCollection) {
+		try {
+			ObjectId collectionId = new ObjectId(id);
+			Optional<Collections> _collection = collectionsRepository.findById(collectionId);
+			if (_collection.isPresent()) {
 				Collections collection = _collection.get();
 				collection.setUser(newCollection.getUser());
 				collection.setCollectionname(newCollection.getCollectionname());
 				collection.setCollectionlist(newCollection.getCollectionlist());
 				collectionsRepository.save(collection);
-				return new ResponseEntity<Collections>(collection,HttpStatus.OK);
-			} else 
-			{
-				return new ResponseEntity<String>("No existe la Colección",HttpStatus.INTERNAL_SERVER_ERROR);
+				return new ResponseEntity<Collections>(collection, HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("No existe la Colección", HttpStatus.INTERNAL_SERVER_ERROR);
 			}
 		} catch (Exception e) {
-			return new ResponseEntity<String>(e.getCause().toString(),HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<String>(e.getCause().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 
